1bc7f22fa8330958cac972988442e200690cfd00
Serena <serenafayelombardo@gmail.com>
2020-07-10 09:38:34 +0200

    Added to cover background effect for mobile open menu

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
 const menuButton = document.querySelector('.header__mobile-menu__button');
 const menuButtonImage = menuButton.querySelector('img');
 const menuBlock = document.querySelector('.header__menu');
+const menuContainer = document.querySelector('.header__menu__container');
 
 menuButton.addEventListener('click', () => {
   if (menuBlock.classList.contains('header__menu__hidden')) {
     menuBlock.classList.remove('header__menu__hidden');
     menuButtonImage.src = 'images/icon-close.svg';
     menuButton.setAttribute('aria-expanded', 'true');
+    menuContainer.classList.add('open');
   } else {
     menuBlock.classList.add('header__menu__hidden');
     menuButtonImage.src = 'images/icon-hamburger.svg';
     menuButton.setAttribute('aria-expanded', 'false');
+    menuContainer.classList.remove('open');
   }
 });
 
@@ -19,5 +22,6 @@ document.addEventListener('keyup', (event) => {
     menuBlock.classList.add('header__menu__hidden');
     menuButtonImage.src = 'images/icon-hamburger.svg';
     menuButton.setAttribute('aria-expanded', 'false');
+    menuContainer.classList.remove('open');
   }
 });
