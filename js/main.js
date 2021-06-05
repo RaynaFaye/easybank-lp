470d77d0bee153482e6ab82a83c4379f221d2168
Serena <serenafayelombardo@gmail.com>
2021-06-05 15:05:37 +0200

    Refactored JS for menu functionality

--- js/main.js
+++ js/main.js
@@ -1,29 +1,33 @@
+//Menu Functionality
 const menuButton = document.querySelector('.header__mobile-menu__button');
 const menuButtonImage = menuButton.querySelector('img');
 const menuBlock = document.querySelector('.header__menu');
 const menuContainer = document.querySelector('.header__menu__container');
 const menuLinks = document.querySelectorAll('.header__menu__link');
 
+function showMenu() {
+  menuBlock.classList.remove('header__menu__hidden');
+  menuButtonImage.src = 'images/icon-close.svg';
+  menuButton.setAttribute('aria-expanded', 'true');
+  menuContainer.classList.add('open');
+}
+function hideMenu() {
+  menuBlock.classList.add('header__menu__hidden');
+  menuButtonImage.src = 'images/icon-hamburger.svg';
+  menuButton.setAttribute('aria-expanded', 'false');
+  menuContainer.classList.remove('open');
+}
 menuButton.addEventListener('click', () => {
   if (menuBlock.classList.contains('header__menu__hidden')) {
-    menuBlock.classList.remove('header__menu__hidden');
-    menuButtonImage.src = 'images/icon-close.svg';
-    menuButton.setAttribute('aria-expanded', 'true');
-    menuContainer.classList.add('open');
+    showMenu();
   } else {
-    menuBlock.classList.add('header__menu__hidden');
-    menuButtonImage.src = 'images/icon-hamburger.svg';
-    menuButton.setAttribute('aria-expanded', 'false');
-    menuContainer.classList.remove('open');
+    hideMenu();
   }
 });
 
 document.addEventListener('keyup', (event) => {
   if (!menuBlock.classList.contains('header__menu__hidden') && event.key === 'Escape') {
-    menuBlock.classList.add('header__menu__hidden');
-    menuButtonImage.src = 'images/icon-hamburger.svg';
-    menuButton.setAttribute('aria-expanded', 'false');
-    menuContainer.classList.remove('open');
+    hideMenu();
   }
 });
 
@@ -32,10 +36,7 @@ menuLinks.forEach((link) => {
     if (event.relatedTarget.classList.contains('header__menu__link')) {
       return;
     } else {
-      menuBlock.classList.add('header__menu__hidden');
-      menuContainer.classList.remove('open');
-      menuButtonImage.src = 'images/icon-hamburger.svg';
-      menuButton.setAttribute('aria-expanded', 'false');
+      hideMenu();
     }
   });
 });
